b20b0607ff5363c13d08268b8fdb1ad212e71d68
Felix Seip <felixseip@Felixs-MacBook-Pro.local>
2019-05-25 11:01:12 +0200

    Initial backend commit

---
+++ backend/code-n-chill/src/main/kotlin/com/codedoor/codenchill/CodeNChillApplication.kt
@@ -0,0 +1,11 @@
+package com.codedoor.codenchill
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodeNChillApplication
+
+fun main(args: Array<String>) {
+	runApplication<CodeNChillApplication>(*args)
+}
---
+++ backend/code-n-chill/src/test/kotlin/com/codedoor/codenchill/CodeNChillApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.codedoor.codenchill
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CodeNChillApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
