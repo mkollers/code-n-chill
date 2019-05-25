8f9883bbfa4ff82233fbc9ec45113e3b51886eff
Markus Kollers <m.kollers@vertical.de>
2019-05-25 14:08:21 +0200

    Cleanup nodes sql

--- database/data/nodes.sql
+++ database/data/nodes.sql
@@ -1,199 +1,138 @@
-DO $$
+INSERT INTO node (content, icon_url) 
+VALUES ('Why do you want to learn programming?', NULL);
 
-BEGIN
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Why do you want to learn programming?', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Make money', NULL);
 
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Make money', NULL);
-    
-    INSERT INTO node (content, icon_url) 
-    VALUES ('For my kids', NULL);
-    
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Start with Scratch, then move on to...', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('For my kids', NULL);
 
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Python', NULL);
-    
-    INSERT INTO node (content, icon_url) 
-    VALUES ('I don''t know just pick one for me', NULL);
-    
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Just for fun', NULL);
-    
-    INSERT INTO node (content, icon_url) 
-    VALUES ('I''m interested', NULL);
-    
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Improve myself', NULL);
-    
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Have a brilliant idea/platform in mind?', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Start with Scratch, then move on to...', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Python', NULL);
 
-    INSERT INTO node (content, icon_url) 
-    VALUES ('<b>Nope.</b> Just want to get started', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('I don''t know just pick one for me', NULL);
 
-    INSERT INTO node (content, icon_url) 
-    VALUES ('I prefer to learn things...', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Just for fun', NULL);
 
-    INSERT INTO node (content, icon_url) 
-    VALUES ('The easy way', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('I''m interested', NULL);
 
-    INSERT INTO node (content, icon_url) 
-    VALUES ('The best way', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Improve myself', NULL);
 
-    INSERT INTO node (content, icon_url) 
-    VALUES ('The slightly harder way', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Auto or manual car?', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Auto', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Manual', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Java', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('C', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Objective-C', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('The really hard way (but easier to pick up other languages in the future)', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('C++', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('YES', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Which platform/field?', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('I have a startup idea!', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Do you want to try something new with huge potential, but less mature?', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('YES', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('NO', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('JavaScript', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Web', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Does your web app provides info in real-time, like twitter?', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('<b>NOT SURE</b>', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('<b>NO</b>', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Which one is your favorite toy?', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Lego', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Play-Doh', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Ruby', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('PHP', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('I''ve an old & ugly toy, but i love it so much!' , NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Get a Job', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Which platform/field?', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('3D/Gaming', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Mobile', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Which OS?', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Android', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('iOS', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Enterprise', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Startup', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('I want to work for...', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Web', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Back-end ("brain" behind a website)', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Front-end (web interface)', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Corporate', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('What do you think about Microsoft?', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('I''m a fan!', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('C#', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Not Bad', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Suck', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Doesn''t matter, I just want \$\$\$', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('I want to work for big tech companies', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Facebook', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Google', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Apple', NULL);
-
-    INSERT INTO node (content, icon_url) 
-    VALUES ('Microsoft', NULL);
-
-END $$;
+INSERT INTO node (content, icon_url) 
+VALUES ('Have a brilliant idea/platform in mind?', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('<b>Nope.</b> Just want to get started', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('I prefer to learn things...', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('The easy way', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('The best way', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('The slightly harder way', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Auto or manual car?', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Auto', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Manual', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Java', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('C', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Objective-C', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('The really hard way (but easier to pick up other languages in the future)', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('C++', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('YES', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Which platform/field?', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('I have a startup idea!', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Do you want to try something new with huge potential, but less mature?', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('YES', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('NO', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('JavaScript', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Web', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Does your web app provides info in real-time, like twitter?', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('<b>NOT SURE</b>', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('<b>NO</b>', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Which one is your favorite toy?', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Lego', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Play-Doh', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Ruby', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('PHP', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('I''ve an old & ugly toy, but i love it so much!' , NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Get a Job', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Which platform/field?', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('3D/Gaming', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Mobile', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Which OS?', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Android', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('iOS', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Enterprise', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Startup', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('I want to work for...', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Web', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Back-end ("brain" behind a website)', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Front-end (web interface)', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Corporate', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('What do you think about Microsoft?', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('I''m a fan!', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('C#', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Not Bad', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Suck', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Doesn''t matter, I just want \$\$\$', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('I want to work for big tech companies', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Facebook', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Google', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Apple', NULL);
+INSERT INTO node (content, icon_url) 
+VALUES ('Microsoft', NULL);
