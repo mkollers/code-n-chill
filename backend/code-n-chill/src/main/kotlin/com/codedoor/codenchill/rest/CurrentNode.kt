0b914b570fb318e2fa61079ae8ce621d71577508
Felix Seip <felixseip@Felixs-MacBook-Pro.local>
2019-05-25 17:07:23 +0200

    Final touches to the rest api

--- backend/code-n-chill/src/main/kotlin/com/codedoor/codenchill/rest/CurrentNode.kt
+++ backend/code-n-chill/src/main/kotlin/com/codedoor/codenchill/rest/CurrentNode.kt
@@ -2,6 +2,17 @@ package com.codedoor.codenchill.rest
 
 import java.util.*
 
-class CurrentNode (id : UUID, content : String, iconUrl:String, answers : List<Children>){
+class CurrentNode {
 
+    var id: UUID?
+    var content: String?
+    var iconUrl: String?
+    var answers: List<Children>?
+
+    constructor(id: UUID?, content: String?, iconUrl: String?, answers: List<Children>?){
+        this.id = id
+        this.content = content
+        this.iconUrl = iconUrl
+        this.answers = answers
+    }
 }
--- backend/code-n-chill/src/main/kotlin/com/codedoor/codenchill/rest_repository/NodeRepository.kt
+++ backend/code-n-chill/src/main/kotlin/com/codedoor/codenchill/rest_repository/NodeRepository.kt
@@ -9,22 +9,36 @@ import java.util.*
 
 @Repository
 interface NodeRepository : CrudRepository<Node, Long> {
-    /***/
+    /*
+     * Get a list of all nodes
+     * */
     @Query(value = "SELECT node FROM Node node")
     fun getAllNodes(): List<Node>
 
+    /*
+     * Get a node by a given UUID
+     * */
     @Override
     @Query(value = "SELECT node FROM Node node WHERE node.id = ?1")
     fun findById(id: UUID): Node
 
+    /*
+    * Get a list of child nodes for a given UUID. Only gets the direct children
+    * */
     @Query(value = "SELECT cn FROM Node node INNER JOIN Edge edge ON edge.primaryKey.previousNode = node.id " +
             "INNER JOIN Node cn ON cn.id = edge.primaryKey.nextNode WHERE node.id = ?1")
     fun getChildNodes(id: UUID): List<Node>
 
+    /*
+    * Get a list of parent nodes for a given UUID. Only gets the direct parents
+    * */
     @Query(value = "SELECT cn FROM Node node INNER JOIN Edge edge ON edge.primaryKey.nextNode = node.id " +
             "INNER JOIN Node cn ON cn.id = edge.primaryKey.previousNode WHERE node.id = ?1")
     fun getParentNodes(id: UUID): List<Node>
 
+    /*
+    * Get the root node of the application
+    * */
     @Query(value = "SELECT * FROM node n LEFT JOIN edge e ON e.fk_next_node = n.pk_id WHERE e.fk_next_node IS NULL", nativeQuery = true)
     fun getRootNode(): Node
 }
