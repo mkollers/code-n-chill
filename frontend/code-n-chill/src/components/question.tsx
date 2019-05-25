9f00d8d845d1ac390082a90098875a991eff4f7b
Alina Burghardt <alina.bgdt@web.de>
2019-05-25 15:14:24 +0200

    changes

--- frontend/code-n-chill/src/components/question.tsx
+++ frontend/code-n-chill/src/components/question.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { RouteComponentProps } from '@reach/router'
+import React, { useEffect, useState } from 'react';
+import { RouteComponentProps } from '@reach/router';
+import Button from './button';
 
 type QuestionProps = RouteComponentProps & {
   id?: number
@@ -7,8 +8,8 @@ type QuestionProps = RouteComponentProps & {
 
 interface IQuestion {
   id: number
-  question: string
-  icon?: string
+  content: string
+  iconUrl: string
   answers: Array<{
     nextId: number,
     text: string
@@ -16,16 +17,28 @@ interface IQuestion {
 }
 
 const Question = ({id}: QuestionProps) => {
-  // useEffect(() => {
-  //   fetch(`http://localhost:3000/nodes/${id}`)
-  //     .then((resp) => {
-  //       console.log(resp)
-  //     })
-  // }, [id])
-
+    const[obj, setObj] = useState({answers:[{ }]} as  IQuestion);
+  useEffect(() => {
+    fetch(`http://localhost:8080/nodes/${id}`)
+      .then((resp) => resp.json())
+      .then((json) => {
+          setObj(json);
+      })
+  }, [id])
+console.log(obj.answers);
+const buttons= []
+var i;
+if (obj.answers !== undefined){
+for (i=0; i<obj.answers.length; i++){
+    buttons.push(<Button key={i} title={obj.answers[i].text} nextId={obj.answers[i].nextId}/>)
+}
+}
   return (
     <div className="question">
-      <h1>{id}</h1>
+      <h1>{obj.content}</h1>
+      {buttons}
+
+      
     </div>
   )
 }
